694f3230b955a2888b927c432b8175c91ee49822
Steven Russell <srussell@buzzardsoft.com>
2019-06-22 16:03:22 -0700

    fixing permissions so that the bsmusic_app user can insert data

--- deployment/local/init.sql
+++ deployment/local/init.sql
@@ -1,10 +1,13 @@
 CREATE ROLE bsmusic_app WITH LOGIN PASSWORD 'bsmusic_password' VALID UNTIL 'infinity' ;
-
-REVOKE CREATE ON DATABASE bsmusic FROM bsmusic_app ;
-
-GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES ON ALL TABLES IN SCHEMA public TO bsmusic_app ;
-GRANT USAGE, SELECT, UPDATE ON ALL sequences IN SCHEMA public TO bsmusic_app ;
 GRANT CONNECT ON DATABASE bsmusic TO bsmusic_app ;
+
+REVOKE CREATE ON schema public FROM public ;
+GRANT CREATE ON schema public TO bsmusic_admin ;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
+    GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES ON TABLES TO bsmusic_app ;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
+    GRANT USAGE, SELECT, UPDATE ON sequences TO bsmusic_app ;
 GRANT TEMP ON DATABASE bsmusic TO bsmusic_app ;
 
 CREATE TABLE IF NOT EXISTS song (
